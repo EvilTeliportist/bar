346faad774898e12bee69a08d5de11f4a605d624
Bryan Houlton <brhoulton@gmail.com>
2020-08-05 17:34:48 -0400

    1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'bar',         # How you named your package folder (MyLib)
-  packages = ['bar'],   # Chose the same as "name"
+  name = 'pyprogbar',         # How you named your package folder (MyLib)
+  packages = ['pyprogbar'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple ProgressBar class',   # Give a short description about your library
